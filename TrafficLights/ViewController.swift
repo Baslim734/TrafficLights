a6450660a0336dce913caa8f3add593df6dd20de
Baslim734 <nikita7343072@gmail.com>
2022-06-19 18:42:06 +0300

    Release of TrafficLight app

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightVIew: UIView!
+    @IBOutlet var greenLightVIew: UIView!
+    @IBOutlet var buttonChangeColor: UIButton!
+    var lightOn = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 80
+        yellowLightVIew.layer.cornerRadius = 80
+        greenLightVIew.layer.cornerRadius = 80
+        buttonChangeColor.layer.cornerRadius = 10
+        redLightView.alpha = 0.3
+        yellowLightVIew.alpha = 0.3
+        greenLightVIew.alpha = 0.3
+        
     }
 
-
+    @IBAction func changeCollorLight(_ sender: Any) {
+        switch lightOn {
+            case 0:
+                redLightView.alpha = 1
+                lightOn = 1
+                buttonChangeColor.setTitle("NEXT", for: .normal)
+            case 1:
+                yellowLightVIew.alpha = 1
+                redLightView.alpha = 0.3
+                lightOn = 2
+            case 2:
+                greenLightVIew.alpha = 1
+                yellowLightVIew.alpha = 0.3
+                lightOn = 3
+            case 3:
+                redLightView.alpha = 1
+                greenLightVIew.alpha = 0.3
+                lightOn = 1
+            default:
+                redLightView.alpha = 1
+                lightOn = 1
+        }
+    }
+    
 }
 
